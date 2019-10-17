e43f57c7e9fb75f03c42ef9e13e7ff1669f8faa0
Valentina Alegría <val.happy.m@gmail.com>
2019-10-17 16:50:29 -0500

    ajustes gg

--- src/containers/Create/Create.js
+++ src/containers/Create/Create.js
@@ -14,7 +14,7 @@ function Create() {
 const useStyles = makeStyles(theme => ({
     container: {
         width: '100%',
-        height: '100%',
+        height: 600,
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
--- src/containers/Garage/Garage.js
+++ src/containers/Garage/Garage.js
@@ -21,7 +21,7 @@ function Garage() {
 const useStyles = makeStyles(theme => ({
     container: {
         width: '100%',
-        height: '100%',
+        height: 600,
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
