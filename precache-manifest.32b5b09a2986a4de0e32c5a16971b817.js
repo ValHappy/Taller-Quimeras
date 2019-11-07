1772681c4831dfeae7f4417c6e4657acd25e80d1
Valentina Alegría <val.happy.m@gmail.com>
2019-11-07 16:57:58 -0500

    Updates

---
+++ precache-manifest.32b5b09a2986a4de0e32c5a16971b817.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13418143290055613cec519e973896ce",
+    "url": "/Taller-Quimeras/index.html"
+  },
+  {
+    "revision": "b0e069c3687bd63d9832",
+    "url": "/Taller-Quimeras/static/css/main.4df5a8a6.chunk.css"
+  },
+  {
+    "revision": "ceb4f7ed10b7f29b9025",
+    "url": "/Taller-Quimeras/static/js/2.d338da18.chunk.js"
+  },
+  {
+    "revision": "b0e069c3687bd63d9832",
+    "url": "/Taller-Quimeras/static/js/main.daac2fbe.chunk.js"
+  },
+  {
+    "revision": "bcb3c0a9062062745a42",
+    "url": "/Taller-Quimeras/static/js/runtime-main.85e94e26.js"
+  },
+  {
+    "revision": "9d42fe02e62fbf3b3f70cc7465a5499a",
+    "url": "/Taller-Quimeras/static/media/alone.9d42fe02.png"
+  },
+  {
+    "revision": "e56796165a1d2125f7f3fcbaa644f623",
+    "url": "/Taller-Quimeras/static/media/background.e5679616.png"
+  },
+  {
+    "revision": "cbb61edd4a78752b33d98ddbe91374fb",
+    "url": "/Taller-Quimeras/static/media/ears_1.cbb61edd.png"
+  },
+  {
+    "revision": "751c48b19ea53bf83e21f534178064ff",
+    "url": "/Taller-Quimeras/static/media/ears_2.751c48b1.png"
+  },
+  {
+    "revision": "22cf22f2e4605b79f784a64d04c51529",
+    "url": "/Taller-Quimeras/static/media/ears_3.22cf22f2.png"
+  },
+  {
+    "revision": "80ea1d11888285ec7817631c510a9404",
+    "url": "/Taller-Quimeras/static/media/ears_5.80ea1d11.png"
+  },
+  {
+    "revision": "3d9cdac4374fee68eff10ae7c48b92ee",
+    "url": "/Taller-Quimeras/static/media/tail_1.3d9cdac4.png"
+  },
+  {
+    "revision": "37c60e2d95999234322ff8b56d2c8b4e",
+    "url": "/Taller-Quimeras/static/media/tail_3.37c60e2d.png"
+  }
+]);
